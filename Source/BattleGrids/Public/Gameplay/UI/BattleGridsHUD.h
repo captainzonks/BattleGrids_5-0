0d36e9f62cf8a52c760356ca2abf5a951d3e7ccf
Matthew Barham <zonks.zonks@gmail.com>
2021-12-20 10:19:10 -0700

    continued initial commits

---
+++ Source/BattleGrids/Public/Gameplay/UI/BattleGridsHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BattleGridsHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEGRIDS_API UBattleGridsHUD : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
